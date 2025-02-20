d14cb6008f2e3441fb034560e58dced6880c5efc
Jonathan Dekhtiar <jonathan@dekhtiar.com>
2025-02-20 03:15:00 -0500

    BugFixes for Demo

--- mockpip/commands/install.py
+++ mockpip/commands/install.py
@@ -72,6 +72,8 @@ def install(args: list[str]) -> int:
     pkg_candidate_dict_by_vhash = {}
     for pkg in pkg_candidates:
         filename = unquote(pkg.filename)
+        if filename[-4:] != ".whl":
+            continue
         logger.info(f"Found: `{filename}`")
         variant_hash = get_variant_hash_from_wheel(filename)
         pkg_candidate_dict_by_vhash[variant_hash] = pkg
--- tests/commands/test_install.py
+++ tests/commands/test_install.py
@@ -8,22 +8,23 @@ from parameterized import parameterized
 
 
 class TestMockpipMain(unittest.TestCase):
-
     def run_command(self, command: str):
         return subprocess.run(  # noqa: S603
-            shlex.split(f"mockpip {command}"),
+            shlex.split(f"mockpip {command}"),  # noqa: S603
             capture_output=True,
             text=True,
-            check=False
+            check=False,
         )
 
-    @parameterized.expand([
-        "",
-        "https://pypi.org/simple",
-        "https://pypi.org/simple/",
-        "https://test.pypi.org/simple",
-        "https://test.pypi.org/simple/",
-    ])
+    @parameterized.expand(
+        [
+            "",
+            "https://pypi.org/simple",
+            "https://pypi.org/simple/",
+            "https://test.pypi.org/simple",
+            "https://test.pypi.org/simple/",
+        ]
+    )
     def test_install_command_pypi(self, index_url):
         """Test the version command to ensure it returns the correct version."""
         index_flag = f"--index-url={index_url}" if index_url else ""
@@ -33,10 +34,19 @@ class TestMockpipMain(unittest.TestCase):
 
         result = self.run_command(f"install requests {index_flag}")
 
-        assert f"Received install request for: `requests` from index: {index_url}" in result.stdout  # noqa: E501
-        assert f"Querying `{index_url.rstrip('/')}/requests/` for package `requests`" in result.stdout  # noqa: E501
-        assert f"Successfully fetched package data from `{index_url.rstrip('/')}/requests/" in result.stdout  # noqa: E501
-        assert "Found: requests" in result.stdout
+        assert (
+            f"Received install request for: `requests` from index: {index_url}"
+            in result.stdout
+        )
+        assert (
+            f"Querying `{index_url.rstrip('/')}/requests/` for package `requests`"
+            in result.stdout
+        )
+        assert (
+            f"Successfully fetched package data from `{index_url.rstrip('/')}/requests/"
+            in result.stdout
+        )
+        assert "Found: `requests" in result.stdout
         assert "Installing: requests" in result.stdout
         assert "[========================================] 100%" in result.stdout
         assert "was installed with success ..." in result.stdout
@@ -47,9 +57,18 @@ class TestMockpipMain(unittest.TestCase):
 
         result = self.run_command(f"install {pkg_name}")
 
-        assert f"Received install request for: `{pkg_name}` from index: https://pypi.org/simple" in result.stdout  # noqa: E501
-        assert f"Querying `https://pypi.org/simple/{pkg_name}/` for package `{pkg_name}`" in result.stdout  # noqa: E501
-        assert f"No candidate found for `{pkg_name}` from `https://pypi.org/simple/{pkg_name}/`" in result.stdout  # noqa: E501
+        assert (
+            f"Received install request for: `{pkg_name}` from index: https://pypi.org/simple"
+            in result.stdout
+        )  # noqa: E501
+        assert (
+            f"Querying `https://pypi.org/simple/{pkg_name}/` for package `{pkg_name}`"
+            in result.stdout
+        )  # noqa: E501
+        assert (
+            f"No candidate found for `{pkg_name}` from `https://pypi.org/simple/{pkg_name}/`"
+            in result.stdout
+        )  # noqa: E501
 
         assert f"No candidate package was found for `{pkg_name}`" in result.stderr
         assert result.returncode != 0
